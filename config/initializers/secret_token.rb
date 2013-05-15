203e4164a29464996db8c0c14f7588e166c1d7ac
Duong Ho Tuan <zamakkat@gmail.com>
2013-05-15 21:39:54 +0800

    Created ROR environment.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieReview::Application.config.secret_token = 'f583750adc01eef220b68cac3ff81da8edfa37da7707698aead30734d6832638594302d210fd0673bb5f78afb0620bd83bccecce355a37dc764ebed6ad299c66'
